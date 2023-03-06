26f73fe2809d586e644548f11c15a63826d8d4c8
Hubbard Harvey III <hcubedcoder@gmail.com>
2023-03-06 14:20:52 -0500

    Add remaps for copy/paste and terminal commands

--- lua/hharvey/remap.lua
+++ lua/hharvey/remap.lua
@@ -11,9 +11,16 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
-
+--Terminal open and resize
+vim.keymap.set("n", "<leader>e", ":vert split | terminal<CR> :vert resize 180<CR>")
+vim.keymap.set("n", "<leader>t", ":botright split | terminal<CR> :resize 20<CR>")
 
 
 -- Remap Terminal Close
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+--stolen from prime time
+vim.keymap.set("x", "<leader>p", "\"_dP")
 
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
