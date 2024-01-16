e795d1a397b16eb391eaf28e7852969491b2b2e0
Hubbard Harvey <hubbard.harvey@gmail.com>
2024-01-16 07:44:27 -0500

    Set tab to 4 spaces, make expandtab false

--- lua/hharvey/set.lua
+++ lua/hharvey/set.lua
@@ -12,10 +12,10 @@ vim.opt.relativenumber = true
 vim.cmd.colorscheme "catppuccin-macchiato"
 vim.opt.termguicolors = true
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
 vim.opt.signcolumn = 'yes'
 
 vim.opt.smartindent = true
